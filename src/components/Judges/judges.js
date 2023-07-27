512064dbe17e8499e65971e74e22f39424a0b7ee
krishnendu roy <krishnendu.kalyani1@gmail.com>
2023-07-27 20:44:18 +0530

    faq and some fix

--- src/components/Judges/judges.js
+++ src/components/Judges/judges.js
@@ -1,140 +1,124 @@
-import React, {useState, useEffect} from 'react';
-import '../Home/home.css'; // Import the CSS file for styling
+import React, { useState, useEffect } from "react";
+import "../Home/home.css"; // Import the CSS file for styling
 import ReactCardCarousel from "react-card-carousel";
 
-import "./judges.css"
-import Judge1 from "./assets/img1.jpg"
-import Judge2 from "./assets/img2.jpg"
-import Judge3 from "./assets/img3.jpg"
-import Judge4 from "./assets/img4.jpg"
-import Judge5 from "./assets/img5.jpg"
-import Judge6 from "./assets/img6.jpg"
-
+import "./judges.css";
+import Judge1 from "./assets/img1.jpg";
+import Judge2 from "./assets/img2.jpg";
+import Judge3 from "./assets/img3.jpg";
+import Judge4 from "./assets/img4.jpg";
+import Judge5 from "./assets/img5.jpg";
+import Judge6 from "./assets/img6.jpg";
 
 const Judges = () => {
-    const CONTAINER_STYLE = {
-        position: "relative",
-        height: "100vh",
-        width: "100%",
-        display: "flex",
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        overflow: "hidden",
-        margin: "5px"
+  const CONTAINER_STYLE = {
+    position: "relative",
+    height: "50vh",
+    width: "100%",
+    display: "flex",
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    overflow: "hidden",
+    margin: "5px",
+  };
 
-    };
+  const CARD_STYLE = {
+    height: "300px",
+    width: "300px",
+    textAlign: "center",
+    background: "#eee",
+    color: "#000",
+    fontFamily: "sans-serif",
+    fontSize: "14px",
+    textTransform: "uppercase",
+    borderRadius: "3px",
+    margin: "0 200px",
+    boxSizing: "border-box",
+  };
 
-    const CARD_STYLE = {
-        height: "300px",
-        width: "300px",
-        textAlign: "center",
-        background: "#eee",
-        color: "#000",
-        fontFamily: "sans-serif",
-        fontSize: "14px",
-        textTransform: "uppercase",
-        borderRadius: "3px",
-        margin: "0 200px",
-        boxSizing: "border-box"
-    };
+  // Array of card data
+  const cardData = [
+    {
+      name: "Jhon Doe",
+      image: Judge1,
+      desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit et cupiditate deleniti.",
+      social: [{ name: "LinkedIn", icon: "fa fa-linkedin" }],
+    },
+    {
+      name: "Jhon Doe",
+      image: Judge2,
+      desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit et cupiditate deleniti.",
+      social: [{ name: "LinkedIn", icon: "fa fa-linkedin" }],
+    },
+    {
+      name: "Jhon Doe",
+      image: Judge3,
+      desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit et cupiditate deleniti.",
+      social: [{ name: "LinkedIn", icon: "fa fa-linkedin" }],
+    },
+    {
+      name: "Jhon Doe",
+      image: Judge4,
+      desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit et cupiditate deleniti.",
+      social: [{ name: "LinkedIn", icon: "fa fa-linkedin" }],
+    },
+    {
+      name: "Jhon Doe",
+      image: Judge5,
+      desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit et cupiditate deleniti.",
+      social: [{ name: "LinkedIn", icon: "fa fa-linkedin" }],
+    },
+    {
+      name: "Jhon Doe",
+      image: Judge6,
+      desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit et cupiditate deleniti.",
+      social: [{ name: "LinkedIn", icon: "fa fa-linkedin" }],
+    },
 
-    // Array of card data
-    const cardData = [
+    // Add more card data objects as needed
+  ];
 
-        {
-            name: "Jhon Doe",
-            image: Judge1,
-            desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit et cupiditate deleniti.",
-            social: [
-                {name: "LinkedIn", icon: "fa fa-linkedin"},
-            ]
-        },
-        {
-            name: "Jhon Doe",
-            image: Judge2,
-            desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit et cupiditate deleniti.",
-            social: [
-                {name: "LinkedIn", icon: "fa fa-linkedin"},
-            ]
-        },
-        {
-            name: "Jhon Doe",
-            image: Judge3,
-            desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit et cupiditate deleniti.",
-            social: [
-                {name: "LinkedIn", icon: "fa fa-linkedin"},
-            ]
-        },
-        {
-            name: "Jhon Doe",
-            image: Judge4,
-            desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit et cupiditate deleniti.",
-            social: [
-                {name: "LinkedIn", icon: "fa fa-linkedin"},
-            ]
-        },
-        {
-            name: "Jhon Doe",
-            image: Judge5,
-            desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit et cupiditate deleniti.",
-            social: [
-                {name: "LinkedIn", icon: "fa fa-linkedin"},
-            ]
-        },
-        {
-            name: "Jhon Doe",
-            image: Judge6,
-            desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit et cupiditate deleniti.",
-            social: [
-                {name: "LinkedIn", icon: "fa fa-linkedin"},
-            ]
-        },
+  return (
+    <div id="Judges">
+      <div className="about-head-section">
+        <h1 className="heading-tag-line">Judges</h1>
+        <hr className="heading-underline" />
+      </div>
+      <div style={CONTAINER_STYLE}>
+        <ReactCardCarousel
+          autoplay={true}
+          autoplay_speed={2500}
+          spread={"wide"}
+          disable_box_shadow={true}
+        >
+          {cardData.map((card, index) => (
+            <div key={index} style={CARD_STYLE}>
+              <div className="card-one">
+                <header>
+                  <div className="avatar">
+                    <img src={card.image} alt={card.name} />
+                  </div>
+                </header>
 
-
-        // Add more card data objects as needed
-    ];
-
-    return (
-        <div>
-            <div className="about-head-section">
-                <h1 className="heading-tag-line">Judges</h1>
-                <hr className="heading-underline"/>
+                <h3>{card.name}</h3>
+                <div className="desc">{card.desc}</div>
+                <div className="contacts">
+                  {/* Render social icons */}
+                  {card.social.map((social, index) => (
+                    <a key={index} href="">
+                      <i className={social.icon}></i>
+                    </a>
+                  ))}
+                  <div className="clear"></div>
+                </div>
+              </div>
             </div>
-            <div style={CONTAINER_STYLE}>
-                <ReactCardCarousel
-                    autoplay={true}
-                    autoplay_speed={2500}
-                    spread={"wide"}
-                    disable_box_shadow={true}
-                >
-                    {cardData.map((card, index) => (
-                        <div key={index} style={CARD_STYLE}>
-                            <div className="card-one">
-                                <header>
-                                    <div className="avatar">
-                                        <img src={card.image} alt={card.name}/>
-                                    </div>
-                                </header>
-
-                                <h3>{card.name}</h3>
-                                <div className="desc">{card.desc}</div>
-                                <div className="contacts">
-                                    {/* Render social icons */}
-                                    {card.social.map((social, index) => (
-                                        <a key={index} href="">
-                                            <i className={social.icon}></i>
-                                        </a>
-                                    ))}
-                                    <div className="clear"></div>
-                                </div>
-                            </div>
-                        </div>
-                    ))}
-                </ReactCardCarousel>
-            </div>
-        </div>
-    );
+          ))}
+        </ReactCardCarousel>
+      </div>
+    </div>
+  );
 };
 
 export default Judges;
